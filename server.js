4d0e6cea216d69ec1f86057168a6fce990670daa
Jlimbu <52299355+JachinLimbu@users.noreply.github.com>
2023-09-10 14:04:36 +0100

    count added

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const PORT = 8080;
 const HOST = '0.0.0.0'
 
+var count=0;
 
 const app = express()
 app.get('/', (req, res)=>{
-  res.send('Hello World')
+  count+=1
+  res.send(`current visits: ${count}`)
 })
 
 app.listen(PORT, HOST, ()=>{
